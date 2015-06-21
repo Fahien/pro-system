de275b607a07203d40d79b18f7f1fe2d7d3bc13f
Volcatius <andrea.carpentiere@hotmail.it>
2015-06-21 18:56:19 +0200

    FormatDAO

---
+++ src/main/java/persistence/FormatDAO.java
@@ -0,0 +1,40 @@
+package persistence;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Logger;
+
+import model.Format;
+
+public class FormatDAO extends AbstractDAO {
+	private static final Logger logger = Logger.getLogger(FormatDAO.class.getName());
+
+	public Format insert(Format format) {
+		PreparedStatement insertFormat = null;
+		String sql = "INSERT INTO format(id , value) ";
+		try {
+			int i = 0;
+			if (format.getId() == 0) {
+				insertFormat = connection.prepareStatement(sql + "VALUES (nextval('formatsequence'), ?)", Statement.RETURN_GENERATED_KEYS);
+			} else {
+				insertFormat = connection.prepareStatement(sql + "VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);
+				insertFormat.setLong(++i, format.getId());
+			}
+			insertFormat.setInt(++i,format.getValue());
+			insertFormat.executeUpdate();
+			if (format.getId() == 0) {
+				ResultSet result = insertFormat.getGeneratedKeys();
+				if (result.next()) {
+					format.setId(result.getLong(1));
+					logger.info("Format id: " + format.getId());
+				}
+			}
+		} catch (SQLException e) {
+			logger.warning(e.getMessage());
+		}
+		return format;
+	}
+
+}
---
+++ src/test/java/persistence/FormatDAOTest.java
@@ -0,0 +1,31 @@
+package persistence;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+
+import model.Format;
+
+import org.junit.Test;
+
+public class FormatDAOTest {
+
+	@Test public void daoTest() {
+		DAO dao = new FormatDAO();
+		Connection connection = dao.getConnection();
+		assertTrue(connection != null);
+		dao.closeConnection();
+	}
+
+	@Test public void insertTest() {
+		Format format = new Format();
+		format.setValue(250);
+
+		FormatDAO dao = new FormatDAO();
+		Connection connection = dao.getConnection();
+		assertTrue (connection != null);
+		format = dao.insert(format);
+		assertFalse(format.getId() == 0);
+		dao.closeConnection();
+	}
+}
